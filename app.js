4c61b1cfd3e18b2dc1321059ae64b2b6422e9ede
Alex Bannow <rbannal@gmail.com>
2019-10-28 15:27:00 -0400

    finished drill 3

--- app.js
+++ app.js
@@ -56,3 +56,62 @@ app.get('/cipher', (req, res) => {
 
   res.send(newArray);
 });
+
+app.get('/lotto', (req, res) => {
+  let numbers = req.query.arr;
+  numbers = numbers.map(num => parseInt(num));
+  if(req.query.arr.length !== 6) {
+    return res.status(400).send('Please input six unique numbers');
+  }
+  numbers.forEach(number => {
+    let repeats = 0;
+    numbers.forEach(compare => {
+      if(compare < 1 || compare >20) {
+        return res.status(400).send('Please input numbers between 1 and 20');
+      }
+      if(compare === number) {
+        repeats = repeats + 1;
+      }
+      if(repeats > 1) {
+        return res.status(400).send('Please input unique numbers');
+      }
+    });
+  });
+  let i = 0;
+  let starterNum = Math.floor(Math.random() * 20) + 1;
+  let winningArr = [starterNum];
+  while(i < 5) {
+    let newNum = Math.floor(Math.random() * 20) + 1;
+
+    if (!winningArr.includes(newNum)) {
+      winningArr.push(newNum);
+      i++;
+    }
+  }
+
+  console.log(winningArr);
+  console.log(numbers);
+
+  let score = 0;
+
+  winningArr.map(number => {
+    if(numbers.includes(number)) {
+      console.log(number);
+      score++;
+    }
+  });
+
+  if(score < 4) {
+    res.send('Sorry, you lose.');
+  }
+  else if(score === 4) {
+    res.send('Congratulations! You win a free ticket.');
+  }
+  else if(score === 5) {
+    res.send('Congratulations! You win $100!');
+  }
+  else if(score === 6) {
+    res.send('Wow! Unbelievable! You could have won the Mega Millions!');
+  }
+});
+
