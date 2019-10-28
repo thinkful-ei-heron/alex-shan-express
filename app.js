521ae439929ab9151706bd36cf56706399dd368c
Alex Bannow <rbannal@gmail.com>
2019-10-28 14:36:27 -0400

    finished second drill

--- app.js
+++ app.js
@@ -5,13 +5,54 @@ const app = express();
 app.use(morgan('dev'));
 
 app.get('/sum', (req, res)=> {
-  const a = req.query.a;
-  const b = req.query.b;
-  const sum = parseInt(a) + parseInt(b);
+  const a = parseInt(req.query.a);
+  const b = parseInt(req.query.b);
+  if(!a || a.isNaN) {
+    return res.status(400).send('Please input a number');
+  }
+  if(!b || b.isNaN) {
+    return res.status(400).send('Please input a number');
+  }
+  const sum = a + b;
   res.send(`The sum of ${a} and ${b} is ${sum}`);
 });
 
 
 app.listen(8000, () => {
   console.log('Express server is listening on port 8000!');
+});
+
+app.get('/cipher', (req, res) => {
+  const text = req.query.text;
+  const shift = parseInt(req.query.shift);
+  const letters = text.split('');
+  let newArray = [];
+
+  letters.map(letter => {
+    if(letter.charCodeAt(0) <= 90 && letter.charCodeAt(0) >= 65) {
+      let newCode = letter.charCodeAt(0) + shift;
+      if(newCode > 90) {
+        newCode = 65 + (newCode - 91);
+        newArray.push(String.fromCharCode(newCode));
+      }
+      else {
+        newArray.push(String.fromCharCode(newCode));
+      } 
+    }
+    else if(letter.charCodeAt(0) <= 122 && letter.charCodeAt(0) >= 97) {
+      let newCode = letter.charCodeAt(0) + shift;
+      if(newCode > 122) {
+        newCode = 97 + (newCode - 123);
+        newArray.push(String.fromCharCode(newCode));
+      }
+      else {
+        newArray.push(String.fromCharCode(newCode));
+      } 
+    }
+    else {
+      newArray.push(letter);
+    }
+  });
+
+  res.send(newArray);
 });
